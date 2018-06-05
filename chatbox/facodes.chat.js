3e4c32b8510a5bf7987eb3505237fcb6d155815a
SSYT 2.0 <SSYT@users.noreply.github.com>
2018-06-05 21:24:38 +0200

    Update facodes.chat.js

--- chatbox/facodes.chat.js
+++ chatbox/facodes.chat.js
@@ -189,8 +189,8 @@ fa_chatbox.prototype.send = function() {
 };
 
 if(typeof faChat == "undefined") {
+    var faChat = new fa_chatbox();
     $(function() {
-        var faChat = new fa_chatbox();
         faChat.init();
         console.log('try login');
         faChat.autologin(_userdata.username);
